06dfc88830490f4f0637e02804690c0393b7ce91
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2022-08-23 21:43:39 +0600

    modal fixed

--- players.js
+++ players.js
@@ -19,9 +19,7 @@ function playerSelected(element) {
         let span = document.getElementsByClassName("close")[0];
 
         // When the user clicks on the button, open the modal
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
+        modal.style.display = "block";
 
         // When the user clicks on <span> (x), close the modal
         span.onclick = function () {
